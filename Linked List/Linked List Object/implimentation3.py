475d0659abaf34fe1d39e71affba34c5dc730482
Enoch Sem <enochsem@gmail.com>
2023-05-22 00:09:05 +0000

    test 3

---
+++ Linked List/Linked List Object/implimentation3.py
@@ -0,0 +1,16 @@
+from linkedlist import LinkedList
+from node import Node
+
+
+llist = LinkedList(['y','o','kk'])
+
+# llist.insertBegining(Node("f"))
+# print(llist)
+# llist.insertEnd(Node("data"))
+# print(llist)
+
+# llist.insertAfter("o", Node("d"))
+# print(llist)
+
+llist.getIndex(1)
+print("hh",llist)
